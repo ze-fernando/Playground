b5d7e0cde4123c7cb381e2ac32fb7b09e7120ed6
José Fernando <zefernando.dev@gmail.com>
2025-01-23 11:48:21 +0000

    Linkedlist insert start and end

--- data_structs/linked_list.c
+++ data_structs/linked_list.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node {
+   int value;
+   struct node *next;
+}Node;
 
-struct node {
-    int value;
-    struct node *next;
-};
+
+Node* create_node(int value){
+    Node* node = malloc(sizeof(Node));
+    node->value = value;
+    node->next = NULL;
+
+    return node;
+}
 
 
-void displayLL(struct node *p){
+void displayLL(Node *p){
     if(p){
         do{
             printf("value: %d ", p->value);
-            printf("| next: %p\n", p->next);
+            printf("| memory adress: %p\n", p->next);
             p=p->next;
         }while(p);
     }
@@ -21,23 +29,43 @@ void displayLL(struct node *p){
     }
 };
 
+Node* insertPrev(Node *head, int value){
+    Node* newNode = malloc(sizeof(Node));
+    newNode->value = value;
+    newNode->next = head;
+
+    return newNode;
+};
+
+
+Node* insertPos(Node *head, int value){
+    Node* newNode = create_node(value);
+     while (head->next != NULL) {
+        head = head->next;
+    }
+    head->next = newNode;
+    newNode->next = NULL;
+
+    return head;
+};
+
 int main() {
-    struct node *node1 = malloc(sizeof(struct node));
-    struct node *node2 = malloc(sizeof(struct node));
-    struct node *node3 = malloc(sizeof(struct node));
-    struct node *node4 = malloc(sizeof(struct node));
+    Node* head = create_node(1);
+    head->next = create_node(2);
+    head->next->next = create_node(3);
+    head->next->next->next = create_node(4);
+
+    printf("\nPrevious list\n");
+    displayLL(head);
+
+    insertPos(head, 5);
+    printf("\nInsert 5 in the end\n");
+    displayLL(head);
     
-    node1->value = 10;
-    node2->value = 20;
-    node3->value = 30;
-    node4->value = 40;
-    
-    node1->next = node2;
-    node2->next = node3;
-    node3->next = node4;
-    node4->next = NULL;
-    
-    
-    displayLL(node1);
+    head = insertPrev(head, 0);
+    printf("\nInsert 0 in the start\n");
+    displayLL(head);
+
+
     return 0;
-}
+};
